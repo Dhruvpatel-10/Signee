fecb1e0cc26d46ab8b66f351ce28f81a4ca648b1
Dhruv <dp397113@gmail.com>
2025-06-26 13:11:57 +0530

    Initial commit for Signee: Frontend

---
+++ ca-dashboard/src/components/nav-main.tsx
@@ -0,0 +1,97 @@
+"use client"
+
+// Collapsible version imports
+
+// import { ChevronRight, type LucideIcon } from "lucide-react"
+// import {
+  //   Collapsible,
+  //   CollapsibleContent,
+  //   CollapsibleTrigger,
+  // } from "@/components/ui/collapsible"
+  
+import { type LucideIcon } from "lucide-react"
+
+import {
+  SidebarGroup,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarMenuSub,
+  SidebarMenuSubButton,
+  SidebarMenuSubItem,
+} from "@/components/ui/sidebar"
+
+
+export function NavMain({
+  items,
+}: {
+  items: {
+    title: string
+    url: string
+    icon?: LucideIcon
+    isActive?: boolean
+    items?: {
+      title: string
+      url: string
+    }[]
+  }[]
+}) {
+  return (
+
+    <SidebarGroup>
+  <SidebarGroupLabel>Certificate Manager</SidebarGroupLabel>
+  <SidebarMenu>
+    {items.map((item) => (
+      <SidebarMenuItem key={item.title}>
+        <SidebarMenuButton asChild>
+          <a href={item.url}>
+            {item.icon && <item.icon />}
+            <span className="text-sm py-0.5">{item.title}</span>
+          </a>
+        </SidebarMenuButton>
+      </SidebarMenuItem>
+    ))}
+  </SidebarMenu>
+</SidebarGroup>
+
+    // Collapsible version with subitems
+
+    // <SidebarGroup>
+    //   <SidebarGroupLabel>Platform</SidebarGroupLabel>
+    //   <SidebarMenu>
+    //     {items.map((item) => (
+    //       <Collapsible
+    //         key={item.title}
+    //         asChild
+    //         defaultOpen={item.isActive}
+    //         className=""
+    //       >
+    //         <SidebarMenuItem>
+    //           <CollapsibleTrigger asChild>
+    //             <SidebarMenuButton tooltip={item.title}>
+    //               {item.icon && <item.icon />}
+    //               <span>{item.title}</span>
+    //               <ChevronRight className="ml-auto transition-transform duration-200 group-data-[state=open]/collapsible:rotate-90" />
+    //             </SidebarMenuButton>
+    //           </CollapsibleTrigger>
+    //           <CollapsibleContent>
+    //             <SidebarMenuSub>
+    //               {item.items?.map((subItem) => (
+    //                 <SidebarMenuSubItem key={subItem.title}>
+    //                   <SidebarMenuSubButton asChild>
+    //                     <a href={subItem.url}>
+    //                       <span>{subItem.title}</span>
+    //                     </a>
+    //                   </SidebarMenuSubButton>
+    //                 </SidebarMenuSubItem>
+    //               ))}
+    //             </SidebarMenuSub>
+    //           </CollapsibleContent>
+    //         </SidebarMenuItem>
+    //       </Collapsible>
+    //     ))}
+    //   </SidebarMenu>
+    // </SidebarGroup>
+  )
+}
